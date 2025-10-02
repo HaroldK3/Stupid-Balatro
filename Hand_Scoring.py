73db9c533e515c9271f1d0db702f51f72ca2300b
HaroldK3 <haroldk@etsu.edu>
2025-10-02 13:52:43 -0400

    Hand_Scoring.py

---
+++ Hand_Scoring.py
@@ -0,0 +1,12 @@
+import Cards
+import requests
+import os
+
+card_values = {
+    "ACE": 14 or 2, "KING": 13, "QUEEN": 12, "JACK": 11, "10": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2
+}
+
+
+def scoring_hand(cards_picked):
+
+    
