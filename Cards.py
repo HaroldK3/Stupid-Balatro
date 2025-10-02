3bd175b25621b092fe848f12e207e2942aad5c4e
HaroldK3 <haroldk@etsu.edu>
2025-10-02 13:31:06 -0400

    Cards.py

--- Cards.py
+++ Cards.py
@@ -45,6 +45,24 @@ def return_cards(deck_id, pile_name):
     data = response.json()
     return data
 
+def select_card(hand, max_cards=5):
+    print("Your hand")
+    for i, card in enumerate(hand, start=1):
+        print(f"{i}: {card['value']} of {card['suit']}")
+
+    while True:
+        print("/nSelect up to 5 cards to play: ")
+        choice = input().strip()
+        try:
+            nums = list(map(int, choice.split()))
+            if 1 <= len(nums) <= max_cards and all(1 <= i <= len(hand) for i in nums):
+                return [hand[choice -1] for i in nums]
+            else: 
+                print("Invalid choice. Try again.")
+        except ValueError:
+            print("Please enter a number.")
+
+
 
 def clear():
     if os.name == 'nt':
--- main.py
+++ main.py
@@ -8,3 +8,21 @@ while not game_over:
     print("--- Welcome to the Stupid Balatro! ---")
     print("1. Start new game")
     print("2. Exit program")
+
+    user_input = int(input())
+
+    if user_input == 1:
+        Cards.clear()
+        done = False
+        deck = Cards.shuffle_new_deck()
+
+        while not done:
+            hand = Cards.draw_card(deck)
+            Cards.select_card(hand)
+
+
+
+    elif user_input == 2:
+        print("Bye bye!!")
+        game_over = True
+
